5bc390e091e20635f31ccdeca1035bdc18239fe4
kehbcw <kehbcw@umsystem.edu>
2020-12-06 19:43:05 -0600

    Updated Main, added GradeLevel

---
+++ Assets/Scripts/DropDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDown : MonoBehaviour
+{
+    public void HandleInputData(int value)
+    {
+        if(value == 0)
+        {
+            PlayerData.Level = 0;
+        }else if(value == 1)
+        {
+            PlayerData.Level = 1;
+        }
+        else if(value == 2)
+        {
+            PlayerData.Level = 2;
+        }
+    }
+}
