f0be54cc39da17d8a2b177979a55a8e07226ef27
jimbotherisenclown <jimbotherisenclown@gmail.com>
2020-12-08 21:35:08 -0600

    Revert "Panels Update - See changelog"
    This reverts commit 896ad214a0e8a7a236debd069d1221bbe4f60953.
    
    Signed-off-by: jimbotherisenclown <jimbotherisenclown@gmail.com>

--- Assets/Scripts/LineScript.cs
+++ Assets/Scripts/LineScript.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class BasicDraw : MonoBehaviour
+public class LineScript : MonoBehaviour
 {
     public GameObject drawToolPrefab; //Holds the imported Unity prefab that is used as a template for how our line should look
     public GameObject currentLine; //This is the Unity object that holds the line that is currently being drawn
@@ -11,33 +10,41 @@ public class BasicDraw : MonoBehaviour
     public List<Vector2> brushPositions; //Holds a list of every position for the current line
     private Sorter sorter; //Holds the imported script used to determine sorting layer for all objects
 
+    // Start is called before the first frame update
     void Start() {
         sorter = GameObject.FindObjectOfType(typeof(Sorter)) as Sorter;
     }
 
+    // Update is called once per frame
     void Update() {
-
-        //If the mouse button is pressed, create a dot sized line
+        //If the mouse button is pressed, create a dot sized line. If the line start has been created, this will not run.
         if (Input.GetMouseButtonDown(0)) {
-            CreateLine();            
+            //Create the start of the line
+            CreateLineStart();
+        }
+        
+        //If the mouse button is pressed, a line start point has been created, and enough time has passed between clicks, create the endpoint for the line
+        if (Input.GetMouseButton(0)) {
+            //Create a variable to hold the current mouse position
+            Vector2 temporaryEndpoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //Set the current mouse position as the temporary endpoint.
+            CreateEndpoint(temporaryEndpoint);
         }
 
-        //While the left mouse button is held down and within canvas borders, set the brush position to the current mouse position and add points to the line
-        if (Input.GetMouseButton(0)) {
-            Vector2 tempBrushPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (Vector2.Distance(tempBrushPosition, brushPositions[brushPositions.Count - 1]) > .1f) {
-                UpdateLineLength(tempBrushPosition);
-            }
-        }
+        /*TO BE IMPLEMENTED:
+        *Check if click is on canvas
+        *If click is off canvas, delete line startpoint.
+        */
     }
 
-    //Create a dot sized line if the player just taps the brush button
-    void CreateLine() {
+    //Unity doesn't pass variables between scripts well, so this is copied wholesale from BasicDraw.cs
+    void CreateLineStart() {
         //Increment the sorting layer by one for all drawn objects
         sorter.setNumberOfItems();
 
         //Create a new line object by using the draw tool prefab with position to be determined later in this function
-        currentLine = Instantiate(drawToolPrefab, Vector3.zero, Quaternion.identity);        
+        currentLine = Instantiate(drawToolPrefab, Vector3.zero, Quaternion.identity);
 
         //Create a line renderer to render the current line
         lineRenderer = currentLine.GetComponent<LineRenderer>();
@@ -60,11 +67,17 @@ public class BasicDraw : MonoBehaviour
         //Sets the end of the brush stroke using the second item in the list
         lineRenderer.SetPosition(1, brushPositions[1]);
     }
+    
 
-    //Adds new points so a line can be created
-    void UpdateLineLength(Vector2 newBrushPosition) {
-        brushPositions.Add(newBrushPosition);
-        lineRenderer.positionCount++;
-        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newBrushPosition);
+    //Create the endpoint for the line
+    void CreateEndpoint(Vector2 endpointPosition) {
+        //Update the lineRenderer to accomodate a third position for the endpoint
+        lineRenderer.positionCount = 3;
+
+        //Set the endpoint to be the current mouse position
+        brushPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        //Add the new endpoint to the lineRenderer list
+        lineRenderer.SetPosition(2, endpointPosition);
     }
 }
